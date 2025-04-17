4073c0ebadb24539af37430a322e66903bc7450d
FireML <fireml@firedev.uk>
2025-04-17 22:36:09 +0100

    Update NBT-API to 2.15.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,7 @@ dependencyResolutionManagement {
             library("griefprevention", "com.github.TechFortress:GriefPrevention:16.17.1")
 
             library("itemsadder-api", "com.github.LoneDev6:API-ItemsAdder:3.6.1")
-            library("nbt-api", "de.tr7zw:item-nbt-api:2.14.2-SNAPSHOT")
+            library("nbt-api", "de.tr7zw:item-nbt-api:2.15.0")
             library("denizen-api", "com.denizenscript:denizen:1.3.1-SNAPSHOT")
             library("oraxen", "io.th0rgal:oraxen:1.188.0")
             library("nexo", "com.nexomc:nexo:1.0.0")
