8145eedd7e59bd927ae6c6733b2f1ed307546722
FireML <fireml@firedev.uk>
2026-01-30 21:13:18 +0000

    Deprecate most EMFAPI methods

--- even-more-fish-plugin/src/main/java/com/oheers/fish/api/EMFAPI.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/api/EMFAPI.java
@@ -12,10 +12,18 @@ import org.jetbrains.annotations.Nullable;
 
 public class EMFAPI {
 
+    /**
+     * @deprecated Use {@link com.oheers.fish.api.fishing.items.AbstractFishManager#isFish(ItemStack)} instead.
+     */
+    @Deprecated(forRemoval = true)
     public boolean isFish(@Nullable ItemStack item) {
         return FishUtils.isFish(item);
     }
 
+    /**
+     * @deprecated Use {@link com.oheers.fish.api.fishing.items.AbstractFishManager#isFish(Skull)} instead.
+     */
+    @Deprecated(forRemoval = true)
     public boolean isFish(@Nullable Skull skull) {
         return FishUtils.isFish(skull);
     }
@@ -24,14 +32,26 @@ public class EMFAPI {
         return FishUtils.isBaitObject(item);
     }
 
+    /**
+     * @deprecated Use {@link com.oheers.fish.api.fishing.items.AbstractFishManager#getFish(ItemStack)} instead.
+     */
+    @Deprecated(forRemoval = true)
     public @Nullable Fish getFish(ItemStack item) {
         return FishUtils.getFish(item);
     }
 
+    /**
+     * @deprecated Use {@link com.oheers.fish.api.fishing.items.AbstractFishManager#getFish(Skull, Player)} instead.
+     */
+    @Deprecated(forRemoval = true)
     public @Nullable Fish getFish(Skull skull, Player fisher) throws InvalidFishException {
         return FishUtils.getFish(skull, fisher);
     }
 
+    /**
+     * @deprecated Use {@link com.oheers.fish.api.fishing.items.AbstractFishManager#getFish(String, String)} instead.
+     */
+    @Deprecated(forRemoval = true)
     public @Nullable Fish getFish(String rarityName, String fishName) {
         return FishManager.getInstance().getFish(rarityName, fishName);
     }
