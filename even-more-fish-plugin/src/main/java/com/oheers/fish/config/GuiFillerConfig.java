2820306480cb88341a8ec78920a85f1f7e488452
FireML <fireml@firedev.uk>
2026-02-17 04:42:04 +0000

    Add create method to EMFGuiItem

--- even-more-fish-plugin/src/main/java/com/oheers/fish/config/GuiFillerConfig.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/config/GuiFillerConfig.java
@@ -1,16 +1,9 @@
 package com.oheers.fish.config;
 
 import com.oheers.fish.EvenMoreFish;
-import com.oheers.fish.FishUtils;
 import com.oheers.fish.api.config.ConfigBase;
+import com.oheers.fish.gui.EMFGui;
 import com.oheers.fish.gui.EMFGuiItem;
-import com.oheers.fish.items.ItemFactory;
-import dev.dejvokep.boostedyaml.block.implementation.Section;
-import dev.triumphteam.gui.BaseGui;
-import dev.triumphteam.gui.paper.Gui;
-import dev.triumphteam.gui.paper.builder.item.ItemBuilder;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -29,52 +22,13 @@ public class GuiFillerConfig extends ConfigBase {
 
     // TODO these were copied from ConfigGui and won't be needed after the switch to Triumph
 
-    public List<EMFGuiItem> getDefaultFillerItems(@NotNull Gui gui) {
+    public List<EMFGuiItem> getDefaultFillerItems(@NotNull EMFGui gui) {
         return getConfig().getRoutesAsStrings(false).stream()
             .map(getConfig()::getSection)
             .filter(section -> section != null && section.contains("item"))
-            .map(section -> getGuiItem(gui, section))
+            .map(section -> EMFGuiItem.create(gui, section))
             .filter(Objects::nonNull)
             .toList();
-    }
-
-    private EMFGuiItem getGuiItem(@NotNull Gui gui, @NotNull Section itemSection) {
-        char character = FishUtils.getCharFromString(itemSection.getString("character", "#"), '#');
-        if (character == '#') {
-            return null;
-        }
-        ItemFactory factory = ItemFactory.itemFactory(itemSection);
-        ItemStack item = factory.createItem();
-        if (item.getType() == Material.AIR) {
-            return null;
-        }
-        return new EMFGuiItem(ItemBuilder.from(item).asGuiItem(), character);
-        /* TODO come back to this once actions are done
-        Section actionSection = itemSection.getSection("click-action");
-        if (actionSection != null) {
-            return new StaticGuiElement(character, item, click -> {
-                BiConsumer<ConfigGui, GuiElement.Click> action = switch (click.getType()) {
-                    case LEFT -> GuiUtils.getActionMap().get(actionSection.getString("left", ""));
-                    case RIGHT -> GuiUtils.getActionMap().get(actionSection.getString("right", ""));
-                    case MIDDLE -> GuiUtils.getActionMap().get(actionSection.getString("middle", ""));
-                    case DROP -> GuiUtils.getActionMap().get(actionSection.getString("drop", ""));
-                    default -> null;
-                };
-                if (action != null) {
-                    action.accept(null, click);
-                }
-                return true;
-            });
-        } else {
-            return new StaticGuiElement(character, item, click -> {
-                BiConsumer<ConfigGui, GuiElement.Click> action = GuiUtils.getActionMap().get(itemSection.getString("click-action", ""));
-                if (action != null) {
-                    action.accept(gui, click);
-                }
-                return true;
-            });
-        }
-         */
     }
 
 }
--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/EMFGuiItem.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/EMFGuiItem.java
@@ -1,8 +1,64 @@
 package com.oheers.fish.gui;
 
+import com.oheers.fish.FishUtils;
+import com.oheers.fish.items.ItemFactory;
+import dev.dejvokep.boostedyaml.block.implementation.Section;
 import dev.triumphteam.gui.element.GuiItem;
+import dev.triumphteam.gui.paper.builder.item.ItemBuilder;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public record EMFGuiItem(@NotNull GuiItem<Player, ItemStack> item, char character) {}
+public record EMFGuiItem(@NotNull GuiItem<Player, ItemStack> item, char character) {
+
+    public static @Nullable EMFGuiItem create(@NotNull EMFGui gui, @NotNull Section section) {
+        char character = FishUtils.getCharFromString(section.getString("character", "#"), '#');
+        if (character == '#') {
+            return null;
+        }
+        ItemFactory factory = ItemFactory.itemFactory(section);
+        ItemStack item = factory.createItem(gui.getPlayer().getUniqueId(), gui.getReplacements());
+        if (item.isEmpty()) {
+            return null;
+        }
+        GuiItem<Player, ItemStack> guiItem = ItemBuilder.from(item).asGuiItem();
+        return new EMFGuiItem(guiItem, character);
+        /* TODO come back to this once actions are done
+        Section actionSection = itemSection.getSection("click-action");
+        if (actionSection != null) {
+            StaticGuiElement actionElement = new StaticGuiElement(character, item, click -> {
+                BiConsumer<ConfigGui, GuiElement.Click> action = switch (click.getType()) {
+                    case LEFT -> actions.get(actionSection.getString("left", ""));
+                    case RIGHT -> actions.get(actionSection.getString("right", ""));
+                    case MIDDLE -> actions.get(actionSection.getString("middle", ""));
+                    case DROP -> actions.get(actionSection.getString("drop", ""));
+                    default -> null;
+                };
+                if (action != null) {
+                    action.accept(this, click);
+                }
+                itemSection.getStringList("click-commands").forEach(command ->
+                    Bukkit.dispatchCommand(click.getWhoClicked(), command)
+                );
+                return true;
+            });
+            gui.addElement(actionElement);
+        } else {
+            StaticGuiElement element = new StaticGuiElement(character, item, click -> {
+                BiConsumer<ConfigGui, GuiElement.Click> action = actions.get(itemSection.getString("click-action", ""));
+                if (action != null) {
+                    action.accept(this, click);
+                }
+                itemSection.getStringList("click-commands").forEach(command ->
+                    Bukkit.dispatchCommand(click.getWhoClicked(), command)
+                );
+                return true;
+            });
+            gui.addElement(element);
+        }
+
+         */
+    }
+
+}
