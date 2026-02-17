90d02e3cde9a39fcfbb34310ac3fb9e9071ffbdd
FireML <fireml@firedev.uk>
2026-02-17 04:44:47 +0000

    Add more methods to EMFGui

--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/EMFGui.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/EMFGui.java
@@ -2,16 +2,24 @@ package com.oheers.fish.gui;
 
 import com.oheers.fish.api.config.ConfigBase;
 import dev.dejvokep.boostedyaml.block.implementation.Section;
+import dev.triumphteam.gui.BaseGui;
+import dev.triumphteam.gui.paper.Gui;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ApiStatus.Internal
 public abstract class EMFGui {
 
     protected final Section config;
     protected final Player player;
+    protected final BaseGui<Player> gui;
+
+    private final Map<String, ?> replacements = new HashMap<>();
 
     public EMFGui(@NotNull Section config, @NotNull HumanEntity human) {
         this.config = config;
@@ -19,12 +27,26 @@ public abstract class EMFGui {
             throw new IllegalStateException("Provided HumanEntity is not a Player.");
         }
         this.player = p;
+        this.gui = createGui();
     }
 
     public abstract void open();
 
     public @NotNull Section getConfig() {
         return this.config;
+    }
+
+    public @NotNull Player getPlayer() {
+        return this.player;
+    }
+
+    public @NotNull Map<String, ?> getReplacements() {
+        return this.replacements;
+    }
+
+    // TODO create Gui and give to GuiReader
+    private @NotNull BaseGui<Player> createGui() {
+        return Gui.of(1).build();
     }
 
 }
--- even-more-fish-plugin/src/main/java/com/oheers/fish/gui/guis/ApplyBaitsGui.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/gui/guis/ApplyBaitsGui.java
@@ -2,7 +2,6 @@ package com.oheers.fish.gui.guis;
 
 import com.oheers.fish.config.GuiConfig;
 import com.oheers.fish.gui.EMFGui;
-import dev.dejvokep.boostedyaml.block.implementation.Section;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
