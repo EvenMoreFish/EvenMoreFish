658003e9f30c4d01b8ba2d600ed72166d3b7aede
FireML <fireml@firedev.uk>
2026-03-11 15:38:00 +0000

    Add MessageRewardBase

--- even-more-fish-plugin/src/main/java/com/oheers/fish/addons/internal/reward/message/MessageRewardBase.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/addons/internal/reward/message/MessageRewardBase.java
@@ -1,4 +1,4 @@
-package com.oheers.fish.addons.internal.reward;
+package com.oheers.fish.addons.internal.reward.message;
 
 import com.oheers.fish.EvenMoreFish;
 import com.oheers.fish.api.reward.RewardType;
@@ -7,30 +7,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import uk.firedev.messagelib.message.ComponentMessage;
+import uk.firedev.messagelib.message.MessageType;
 
-public class MessageRewardType extends RewardType {
+/**
+ * Base for all Message-based reward types.
+ * Handles {@link #doReward(Player, String, String, Location)} and {@link #getPlugin()}
+ */
+abstract class MessageRewardBase extends RewardType {
 
     @Override
     public void doReward(@NotNull Player player, @NotNull String key, @NotNull String value, Location hookLocation) {
-        ComponentMessage.componentMessage(value)
+        ComponentMessage.componentMessage(value, getMessageType())
             .replace("{player}", player.name())
             .parsePlaceholderAPI(player)
             .send(player);
     }
 
     @Override
-    public @NotNull String getIdentifier() {
-        return "MESSAGE";
-    }
-
-    @Override
-    public @NotNull String getAuthor() {
-        return "Oheers";
-    }
-
-    @Override
     public @NotNull JavaPlugin getPlugin() {
         return EvenMoreFish.getInstance();
     }
+
+    public abstract @NotNull MessageType getMessageType();
 
 }
