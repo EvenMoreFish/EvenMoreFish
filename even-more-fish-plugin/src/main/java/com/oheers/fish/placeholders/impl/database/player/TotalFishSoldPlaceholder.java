e6c2d20844853d67767271ac59104badf917287f
FireML <fireml@firedev.uk>
2026-03-30 00:11:54 +0100

    Rewrite total_competitions_joined

--- even-more-fish-plugin/src/main/java/com/oheers/fish/placeholders/impl/database/player/TotalFishSoldPlaceholder.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/placeholders/impl/database/player/TotalFishSoldPlaceholder.java
@@ -1,15 +1,6 @@
 package com.oheers.fish.placeholders.impl.database.player;
 
-import com.oheers.fish.api.Logging;
-import com.oheers.fish.database.model.user.UserReport;
-import com.oheers.fish.placeholders.abstracted.EMFPlaceholder;
 import com.oheers.fish.placeholders.abstracted.UserReportPlaceholder;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.UUID;
-import java.util.function.Function;
 
 public class TotalFishSoldPlaceholder extends UserReportPlaceholder {
 
