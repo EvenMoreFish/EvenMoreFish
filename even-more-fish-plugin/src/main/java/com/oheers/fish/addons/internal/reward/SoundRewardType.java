4a45474c179321cf3ed83a0b01c86b56215482f7
FireML <fireml@firedev.uk>
2026-07-03 00:15:37 +0100

    Use SoundSerializer in SoundRewardType

--- even-more-fish-plugin/src/main/java/com/oheers/fish/addons/internal/reward/SoundRewardType.java
+++ even-more-fish-plugin/src/main/java/com/oheers/fish/addons/internal/reward/SoundRewardType.java
@@ -3,6 +3,7 @@ package com.oheers.fish.addons.internal.reward;
 import com.oheers.fish.EvenMoreFish;
 import com.oheers.fish.FishUtils;
 import com.oheers.fish.api.reward.RewardType;
+import com.oheers.fish.config.serializer.SoundSerializer;
 import net.kyori.adventure.sound.Sound;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -13,29 +14,7 @@ public class SoundRewardType extends RewardType {
 
     @Override
     public void doReward(@NotNull Player player, @NotNull String key, @NotNull String value, Location hookLocation) {
-        String[] split = value.split(",");
-        Sound.Type soundType = FishUtils.getSound(split[0]);
-        if (soundType == null) {
-            EvenMoreFish.getInstance().getLogger().warning("Invalid sound specified for RewardType " + getIdentifier() + ": " + split[0]);
-            return;
-        }
-        float volume;
-        try {
-            volume = Float.parseFloat(split[1]);
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
-            volume = 1.0f;
-        }
-        float pitch;
-        try {
-            pitch = Float.parseFloat(split[2]);
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
-            pitch = 1.0f;
-        }
-        Sound sound = Sound.sound()
-            .type(soundType)
-            .volume(volume)
-            .pitch(pitch)
-            .build();
+        Sound sound = SoundSerializer.deserialize(value);
         player.playSound(sound, Sound.Emitter.self());
     }
 
