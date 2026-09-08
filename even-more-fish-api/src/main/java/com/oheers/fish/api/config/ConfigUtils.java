47c156ee8dbc64041dc03067196f51f4cd7ecd8a
FireML <fireml@firedev.uk>
2026-09-08 23:47:29 +0100

    Fix ConfigUtils

--- even-more-fish-api/src/main/java/com/oheers/fish/api/config/ConfigUtils.java
+++ even-more-fish-api/src/main/java/com/oheers/fish/api/config/ConfigUtils.java
@@ -1,5 +1,6 @@
 package com.oheers.fish.api.config;
 
+import dev.dejvokep.boostedyaml.block.implementation.Section;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
@@ -24,6 +25,30 @@ public class ConfigUtils {
     public static @Nullable ConfigurationSection getSectionOfMany(@NonNull ConfigurationSection section, @NonNull String... paths) {
         for (String path : paths) {
             ConfigurationSection sub = section.getConfigurationSection(path);
+            if (sub != null) {
+                return sub;
+            }
+        }
+        return null;
+    }
+
+    // BoostedYAML - Soon to be removed.
+
+    public static @NonNull Section getOrCreateSection(@NonNull Section section, @NonNull String path) {
+        Section subSection = section.getSection(path);
+        if (subSection == null) {
+            subSection = section.createSection(path);
+        }
+        return subSection;
+    }
+
+    /**
+     * Gets the first section of many paths.
+     * Useful for typos... Oops
+     */
+    public static @Nullable Section getSectionOfMany(@NonNull Section section, @NonNull String... paths) {
+        for (String path : paths) {
+            Section sub = section.getSection(path);
             if (sub != null) {
                 return sub;
             }
--- versions/26-3/build.gradle.kts
+++ versions/26-3/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     compileOnly(project(":even-more-fish-plugin"))
 
     compileOnly("io.papermc.paper:paper-api:26.3-pre-2.build.0-alpha")
-    compileOnly(rootProject.files("temp-libs/paper-26.3.jar")) // DO NOT PUSH TO GITHUB
+    compileOnly(rootProject.files("temp-libs/paper-26.3.jar")) // DO NOT PUSH JAR FILE TO GITHUB
     //paperweight.paperDevBundle("26.3-pre-2.build.0-alpha")
 
     compileOnly(libs.jspecify)
